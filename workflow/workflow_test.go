9a77a4e8bc2cec966a694636212a23a24d8e1170
Eric Caleb <eric@weaviate.io>
2024-11-20 17:11:45 +0100

    fix list pods workflow

--- workflow/workflow_test.go
+++ workflow/workflow_test.go
@@ -4,8 +4,8 @@ import (
 	"testing"
 
 	"github.com/iamcaleberic/tempo/activity"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/require"
 	"go.temporal.io/sdk/testsuite"
 )
 
@@ -15,13 +15,13 @@ func Test_ListPodsWorkflow(t *testing.T) {
 	env := testSuite.NewTestWorkflowEnvironment()
 
 	// Mock activity implementation
-	env.OnActivity(activity.ListPods, mock.Anything).Return([]string{"test-pod-1", "test-pod-2"}, nil)
+	env.OnActivity(activity.ListPods, mock.Anything, activity.ListPodsObject{}).Return([]string{"test-pod-1", "test-pod-2"}, nil)
 
 	env.ExecuteWorkflow(ListPodsWorkflow)
-	require.True(t, env.IsWorkflowCompleted())
-	require.NoError(t, env.GetWorkflowError())
+	assert.True(t, env.IsWorkflowCompleted())
+	assert.NoError(t, env.GetWorkflowError())
 
 	var podNames []string
-	require.NoError(t, env.GetWorkflowResult(&podNames))
-	require.Equal(t, []string{"test-pod-1", "test-pod-2"}, podNames)
+	assert.NoError(t, env.GetWorkflowResult(&podNames))
+	assert.Equal(t, []string{"test-pod-1", "test-pod-2"}, podNames)
 }
